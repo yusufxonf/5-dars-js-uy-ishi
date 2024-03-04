c9f046ccac0ff10cc52ecb2524840caaaf72b946
Yusufkhan <154424859+yusufxonf@users.noreply.github.com>
2024-03-04 14:10:14 +0500

    salom

---
+++ app.js
@@ -0,0 +1,199 @@
+
+// var haqida
+var name1 = "shodibek"
+console.log(name1);
+var name2 = "dovronbek"
+console.log(name2);
+
+var name3 = "yusuf"
+console.log(name3);
+var name4 = "ramozon"
+console.log(name4);
+
+var name4 = "isobek"
+console.log(name4);
+var name5 = "behruz"
+console.log(name5);
+
+var name6 = "laziz"
+console.log(name6);
+var name4 = "ramozon"
+console.log(name4);
+
+var name7 = "aziz"
+console.log(name7);
+var name8 = "umidjon"
+console.log(name8);
+
+
+//let haqida
+let name9 = "1"
+name9 = "1"
+console.log(name9);
+
+let name10 = "2"
+name10 = "2"
+console.log(name10);
+
+let name11 = "3"
+name11 = "3"
+console.log(name11);
+
+
+let name12 = "4"
+name12 = "4"
+console.log(name12);
+
+let name13 = "5"
+name13 = "5"
+console.log(name13);
+
+let name14 = "6"
+name14 = "6"
+console.log(name14);
+
+let name15 = "7"
+name15 = "7"
+console.log(name15);
+
+let name16 = "8"
+name16 = "8"
+console.log(name16);
+
+let name17 = "9"
+name17 = "9"
+console.log(name17);
+
+let name18 = "10"
+name18 = "10"
+console.log(name18);
+
+
+//const haqida
+const age = 18;
+console.log(age);
+
+const ty = 19;
+console.log(ty);
+
+const salom = 20;
+console.log(20);
+
+const Hello = 21;
+console.log(21);
+
+const Welcome = 22;
+console.log(22);
+
+const world = 23;
+console.log(23);
+
+const alik = 24;
+console.log(24);
+
+const hush = 25;
+console.log(25);
+
+const aa = 26;
+console.log(26);
+
+const ss = 27;
+console.log(27);
+
+const qq = 28;
+console.log(28);
+
+
+//alert haqida
+alert ("hello_1");
+alert("hello_2");
+alert("hello_3");
+alert("hello_4");
+alert("hello_5");
+
+
+//confirm haqida
+confirm("shodibek_1");
+confirm("shodibek_2");
+confirm("shodibek_3");
+confirm("shodibek_4");
+confirm("shodibek_5");
+
+
+//prompt haqida
+prompt("Ener your name_1");
+prompt("Ener your name_2");
+prompt("Ener your name_3");
+prompt("Ener your name_4");
+prompt("Ener your name_5");
+
+
+
+
+
+
+
+
+
+
+
+// alert('Hello guys');
+//habar beradi
+
+// confirm("Welcome to site");
+// ogohlantirish
+
+// prompt("Enter your name!");
+// savolar bersa buladi
+
+// let var const
+// name1 = "davronbek";
+
+// var name1 = "bexruz";
+
+// console.log(name1);
+
+// let name1 = "shakhruz";
+// name1 = "yusuf";
+// console.log(name1);
+// number = 0987654321;
+// console.log(number);
+
+// const number = "2003.06.25";
+
+// const => uzgarmas
+// var => varibels
+
+// const age = 20;
+// const name = "shakhruz";
+// const surname = "ismoilov";
+
+// const respons =
+//   "Mening ismim " +
+//   name +
+//   " Mening familyam " +
+//   surname +
+//   " " +
+//   "Mening yoshim " +
+//   age +
+//   ".";
+
+// console.log(respons);
+
+// console.log(2 / "2");
+// console.log("2" - 2);
+// console.log("2" / 2);
+// console.log("2" * 2);
+
+// var => 10
+// let => 10
+// const => 10
+// alert => 5
+// confirm => 5
+// prompt => 5
+// console.log(); => natijalar chiqishi kerak
+
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = "salom";
+
